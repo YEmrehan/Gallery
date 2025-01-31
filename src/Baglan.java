3031bffbdd27da9916897ef951dcd34c5c65aebb
Emrehan Yurdakul <yurdakulemrehan@gmail.com>
2025-01-31 23:22:23 +0300

    PascalCase

---
+++ src/Baglan.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Baglan {
+
+    public Connection baglan() {
+        String yol = "jdbc:mysql://localhost:3308/gallery";
+        String kullanici_adi = "root";
+        String parola = "";
+        Connection conn = null;
+        try {
+            // Veritabanı bağlantısını kurmaya çalışıyoruz
+            conn = DriverManager.getConnection(yol, kullanici_adi, parola);
+            if (conn != null) {
+                System.out.println("Veritabanı bağlantısı başarılı.");
+            }
+        } catch (SQLException e) {
+            // Hata durumunda kullanıcıya mesaj gösteriyoruz
+            JOptionPane.showMessageDialog(null, "Veritabanı Bağlantısı Sırasında Bir Hata Oluştu: " + e.getMessage());
+        }
+        return conn;
+    }
+}
